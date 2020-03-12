c87a1959a38831bdb4c17908dd9d34f75c0e4025
Konstantin S. Vishnivetsky <voip-projects@vishnivetsky.ru>
2020-03-13 05:09:55 +0700

    Working get_xml_value API application

--- mod_parser.c
+++ mod_parser.c
@@ -43,12 +43,92 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_parser_load);
  */
 SWITCH_MODULE_DEFINITION(mod_parser, mod_parser_load, mod_parser_shutdown, NULL);
 
-SWITCH_STANDARD_APP(get_json_value_function)
+/*
+SWITCH_STANDARD_API(get_json_value_function)
 {
-}
+	const char * value = NULL;
+	int tagc = 0;
+	char *mydata = NULL, *tagv[1024] = { 0 } ;
+	int i = 0;
+	const char *err = NULL;
+	switch_xml_t xml_root = NULL, xml = NULL, obj = NULL;
 
-SWITCH_STANDARD_APP(get_xml_value_function)
+	if (!cmd) {
+		err = "Usage: get_json_value <path>";
+		goto end;
+	}
+
+	mydata = strdup(cmd);
+	switch_assert(mydata);
+	tagc = switch_separate_string(mydata, ' ', tagv, (sizeof(tagv) / sizeof(tagv[0])));
+
+	if (tagc == 0) {
+		err = "Usage: get_json_value <path>";
+		goto end;
+	}
+
+end:
+
+	free(mydata);
+	return SWITCH_STATUS_SUCCESS;
+}
+*/
+
+SWITCH_STANDARD_API(get_xml_value_function)
 {
+	const char * value = NULL;
+	int tagc = 0;
+	char *mydata = NULL, *tagv[1024] = { 0 } ;
+	int i = 0;
+	const char *err = NULL;
+	switch_xml_t xml_root = NULL, xml = NULL, obj = NULL;
+
+	if (!cmd) {
+		err = "Usage: get_xml_value <xml> <tag[@param[@value]]> [tag1[@param1[@value1]]] ... <@attribute>";
+		goto end;
+	}
+
+	mydata = strdup(cmd);
+	switch_assert(mydata);
+	tagc = switch_separate_string(mydata, ' ', tagv, (sizeof(tagv) / sizeof(tagv[0])));
+
+	if (tagc < 2) {
+		err = "Usage: get_xml_value <xml> <tag[@param[@value]]> [tag1[@param1[@value1]]] ... <@attribute>";
+		goto end;
+	}
+
+	if (!(xml_root = xml = switch_xml_parse_str_dynamic(tagv[0], SWITCH_TRUE))) {
+	    err = "Error parsing XML data";
+	    goto end;
+	}
+
+	for(i=1; i<tagc; i++) {
+		value = getXMLNode(tagv[i], xml, &obj);
+
+		if (obj == NULL) {
+			obj = xml;
+			goto end;
+		} else {
+			xml = obj;
+		}
+	}
+
+end:
+	if (err) {
+		stream->write_function(stream, "-ERR %s\n", err);
+	}
+
+	if (obj) {
+		//TODO: return real result
+		stream->write_function(stream, "%s", value);
+	} else {
+		err = "Found nothing.";
+		stream->write_function(stream, "-ERR %s\n", err);
+	}
+
+	switch_xml_free(xml_root);
+	free(mydata);
+	return SWITCH_STATUS_SUCCESS;
 }
 
 SWITCH_STANDARD_API(get_config_value_function)
@@ -110,7 +190,6 @@ end:
 SWITCH_MODULE_LOAD_FUNCTION(mod_parser_load)
 {
 	// TODO: define used interfaces
-	switch_application_interface_t *app_interface;
 	switch_api_interface_t *api_interface;
 
 	/* connect my internal structure to the blank pointer passed to me */
@@ -118,10 +197,8 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_parser_load)
 
 	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, "Module mod_parser loading...\n");
 
-	SWITCH_ADD_APP(app_interface, "get_json_value", "Get value from JSON object, encoded to string", "Get value from JSON object, encoded to string", get_json_value_function, "<JSON> <path_to_value>", SAF_NONE);
-	SWITCH_ADD_APP(app_interface, "get_xml_value", "Get value from XML document, encoded to string", "Get value from XML document, encoded to string", get_xml_value_function, "<XML> <path_to_value>", SAF_NONE);
-//	SWITCH_ADD_APP(app_interface, "get_config_value", "Get value from XML config", "Get value from XML config", get_config_value_function, "<section> <path_to_value>", SAF_NONE);
-
+//	SWITCH_ADD_API(api_interface, "get_json_value", "Get value from JSON object, encoded to string", get_json_value_function, "<JSON> <path_to_value>");
+	SWITCH_ADD_API(api_interface, "get_xml_value", "Get value from XML document, encoded to string", get_xml_value_function, "<XML> <path_to_value>");
 	SWITCH_ADD_API(api_interface, "get_config_value", "Get value from XML config", get_config_value_function, "<section> <path_to_value>");
 
 	/* indicate that the module should continue to be loaded */
